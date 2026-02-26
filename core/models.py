d1fef014210de142250ceaeb598b8fc04199d829
astannnn <strong.astan@gmail.com>
2026-02-26 23:03:06 +0100

    Update models.py

--- core/models.py
+++ core/models.py
@@ -2,15 +2,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class SimpleCNN(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes=10):
         super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, 3)
         self.conv2 = nn.Conv2d(32, 64, 3)
         self.pool = nn.MaxPool2d(2)
         self.fc1 = nn.Linear(64 * 6 * 6, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
