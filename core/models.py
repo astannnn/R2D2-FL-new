914fc602b72c889c016d519f23678479759134a6
astannnn <strong.astan@gmail.com>
2026-02-26 23:15:44 +0100

    Update models.py

--- core/models.py
+++ core/models.py
@@ -2,12 +2,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class SimpleCNN(nn.Module):
-    def __init__(self, num_classes=10):
+    def __init__(self, num_classes, in_channels):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3)
+
+        self.conv1 = nn.Conv2d(in_channels, 32, 3)
         self.conv2 = nn.Conv2d(32, 64, 3)
         self.pool = nn.MaxPool2d(2)
+
         self.fc1 = nn.Linear(64 * 6 * 6, 128)
         self.fc2 = nn.Linear(128, num_classes)
 
