73b9fbc335c7de5e8279f351f0db27799a5001a4
astannnn <strong.astan@gmail.com>
2026-02-27 11:09:11 +0100

    Update config.py

--- config.py
+++ config.py
@@ -13,20 +13,20 @@ class Config:
     # =====================================================
     # FL setup
     # =====================================================
-    NUM_CLIENTS = 8
-    CLIENT_FRACTION = 0.75
-    LOCAL_EPOCHS = 2
+    NUM_CLIENTS = 1
+    CLIENT_FRACTION = 1.0
+    LOCAL_EPOCHS = 1
     BATCH_SIZE = 128
-    ROUNDS = 30
-    LR = 0.01
-    DIRICHLET_ALPHA = 0.3
+    ROUNDS = 5
+    LR = 0.001
+    DIRICHLET_ALPHA = 0.5
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
     # =====================================================
     # Noise setup
     # =====================================================
     NOISE_CLIENT_RATIO = 0.5
-    NOISE_RATE = 0.4
+    NOISE_RATE = 0.0
     NOISE_TYPE = "symmetric"   # "symmetric" / "heterogeneous"
 
     # =====================================================
@@ -42,7 +42,7 @@ class Config:
     # =====================================================
     # R2D2-FL parameters
     # =====================================================
-    USE_R2D2 = True
+    USE_R2D2 = False
 
     TEMPERATURE = 2.0
     BETA = 0.1
