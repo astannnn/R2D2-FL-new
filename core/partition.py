59ab164b02be95abedee9f3716838a1ecb7e73dc
astannnn <strong.astan@gmail.com>
2026-03-06 02:43:37 +0100

    Update partition.py

--- core/partition.py
+++ core/partition.py
@@ -2,15 +2,23 @@ import numpy as np
 
 
 def dirichlet_partition(labels, num_clients, alpha):
+
     num_classes = np.unique(labels).shape[0]
 
     label_indices = [np.where(labels == i)[0] for i in range(num_classes)]
+
     client_indices = [[] for _ in range(num_clients)]
 
     for c in range(num_classes):
+        np.random.shuffle(label_indices[c])
+
+    for c in range(num_classes):
+
         proportions = np.random.dirichlet(
             np.repeat(alpha, num_clients)
         )
+
+        proportions = proportions / proportions.sum()
 
         proportions = (
             np.cumsum(proportions) * len(label_indices[c])
@@ -18,7 +26,10 @@ def dirichlet_partition(labels, num_clients, alpha):
 
         split = np.split(label_indices[c], proportions)
 
-        for i, idx in enumerate(split):
-            client_indices[i].extend(idx.tolist())
+        for i in range(num_clients):
+            client_indices[i].extend(split[i].tolist())
+
+    for i in range(num_clients):
+        np.random.shuffle(client_indices[i])
 
     return client_indices
